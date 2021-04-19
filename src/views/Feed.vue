459c9aff1b4783329d599ed1d04e071ef2de6b92
Grégory <milton-the-wizard@hotmail.fr>
2021-04-19 11:02:31 +0200

    modif page feed

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -3,11 +3,14 @@
     <Header />
     <main class="mainFeed">
       <h1>Fil d'actualité</h1>
-      <article class="postArticle">
+      <article class="postArticle" v-for="post in allPosts" :key="post.id">
         <header>
           <div class="infos">
-            <span>Lokkotara</span>
-            <span>15/04/2021</span>
+            <div class="subInfos">
+              <img src="../assets/femme_au_voile1618230036501.jpg" alt="" />
+              <span>Sarah / {{ post.idUser }}</span>
+            </div>
+            <span class="date">{{ moment(post.createdAt).fromNow() }}</span>
           </div>
           <div class="icons">
             <span class="fas fa-heart"></span>
@@ -16,19 +19,16 @@
         </header>
         <div class="content">
           <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. A suscipit
-            necessitatibus unde enim vel repellat doloremque dignissimos nobis
-            omnis. Quas suscipit incidunt fuga maiores tenetur delectus possimus
-            impedit? Enim, animi?
+            {{ post.content }}
           </p>
         </div>
         <footer>
           <div class="numbers">
-            <span>18 likes</span>
-            <span>3 commentaires</span>
+            <span>{{ post.likes }} likes</span>
+            <span>{{ post.comments }} commentaires</span>
           </div>
           <div class="commentLine">
-            <img src="../assets/femme_au_voile1618230036501.jpg" alt="" />
+            <img src="../assets/beau_gosse1617867815195.jpg" alt="" />
             <input type="text" placeholder="Votre commentaire..." />
             <span class="fas fa-chevron-right validate"></span>
           </div>
@@ -40,78 +40,52 @@
 
 <script>
 import Header from "../components/Header.vue";
+const moment = require("moment");
+import axios from "axios";
 export default {
   name: "Feed",
   components: {
     Header,
   },
-  props: {
-    msg: String,
+  data() {
+    let user = JSON.parse(localStorage.getItem("user"));
+    return {
+      moment: moment,
+      token: "",
+      userId: user.id,
+      isAdmin: "",
+      allPosts: [],
+    };
+  },
+  methods: {
+    getAllPosts() {
+      axios
+        .get("http://localhost:3000/api/feed/")
+        .then((res) => {
+          this.allPosts = res.data;
+        })
+        .catch((error) => {
+          console.log({ error });
+        });
+    },
+  },
+  mounted() {
+    this.getAllPosts();
   },
 };
 </script>
 
 <style scoped lang="scss">
-.mainHeader {
-  width: 100%;
-  background-color: rgba(35, 49, 73, 0.972);
-  background-image: linear-gradient(
-    315deg,
-    #4f6791 0%,
-    rgba(35, 49, 73, 0.972) 74%
-  );
-  color: #fff2f2;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  padding: 2rem 0;
-  img {
-    height: 5rem;
-  }
-}
-.profileBtn {
-  display: flex;
-  align-items: center;
-  padding: 0.5rem 1.5rem;
-  img {
-    width: 5rem;
-    height: 5rem;
-    object-fit: cover;
-    object-position: center;
-    border-radius: 50%;
-    margin-left: 1rem;
-  }
-  .fa-sort-down {
-    font-size: 2.5rem;
-    margin-left: 1.5rem;
-    border-radius: 50%;
-    text-align: center;
-  }
-}
-.dropDown {
-  margin-left: auto;
-  display: flex;
-  text-align: center;
-  flex-direction: column;
-  justify-content: center;
-  width: max-content;
-  padding: 0 2rem 2rem 1rem;
-  color: #fff2f2;
-  background-image: linear-gradient(
-    315deg,
-    #4f6791 0%,
-    rgba(35, 49, 73, 0.972) 74%
-  );
-}
-nav ul {
-  display: flex;
-  flex-direction: column;
-}
-.commentLine img {
+.commentLine img,
+.infos img {
   width: 3rem;
   height: 3rem;
+  object-fit: cover;
   margin: 0.5rem 1rem;
   border-radius: 50%;
+}
+.date {
+  margin: 0 1rem;
 }
 .postArticle {
   border-radius: 2rem;
@@ -125,6 +99,9 @@ nav ul {
   align-items: center;
   .content {
     padding: 0 1.5rem;
+    p {
+      text-overflow: fade;
+    }
   }
   header {
     border-radius: 2rem 2rem 0 0;
@@ -143,6 +120,10 @@ nav ul {
     .infos {
       display: flex;
       flex-direction: column;
+      .subInfos {
+        display: flex;
+        align-items: center;
+      }
     }
     .icons {
       display: flex;
